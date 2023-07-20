6659d8038e42967d2d504e19cc02fd0943796d9d
Mateo071 <floresmateo2000@gmail.com>
2023-07-20 14:31:49 -0400

    Completed steps 4-6

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ class Car {
   }
 
   getDescription() {
-    return `
-    Make: ${this.make}, Model: ${this.model}, Year: ${this.year}
-    `;
+    return `Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`;
+  }
+}
+
+class ElectricCar extends Car {
+  constructor(make, model, year, range) {
+    super(make, model, year);
+    this.range = range;
+  }
+
+  getDescription() {
+    return `${super.getDescription()}, Range: ${this.range} miles`
   }
 }
 
 const myCar = new Car (`Honda`, `Civic`, `2023`);
-console.log(myCar.getDescription())
+console.log(myCar.getDescription());
+
+const myOtherCar = new ElectricCar (`Tesla`, `Model S`, `2019`, 300);
+console.log(myOtherCar.getDescription());
